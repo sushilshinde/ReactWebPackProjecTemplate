170d370d411ee7247215c80779529f0bf73b25f0
Sushil From Win10 <shinde.sushil@gmail.com>
2017-07-03 17:52:21 +0530

    Commiting

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,6 +1,47 @@
 import React from "react";
 
 export class Home extends React.Component {
+ 
+
+    constructor(props){
+        super();
+        console.log("In constructor");
+    }
+
+    componentWillMount() {
+        console.log("componentWillMount")
+    }
+    componentDidMount() {
+        console.log("componentDidMount")
+    }
+    
+    componentWillReceiveProps(nP){
+        console.log("componentWillRecieveProps")
+        console.log(nP)
+    }
+    compoenentShoudUpdate(nP,nS){
+        console.log("compoenentShoudUpdate")
+        console.log(nP)
+        console.log(nS)
+        return true;
+    }
+
+    componentWillUpdate(nP,nS){
+        console.log("componentWillUpdate")
+        console.log(nP)
+        console.log(nS)
+    }
+
+    componentDidUpdate(nP,nS){
+        console.log("componentDidUpdate")
+        console.log(nP)
+        console.log(nS)
+    }
+    
+    componentWillUnmont(){
+        console.log("componentWillUnmont")        
+    }
+
     render() { 
     	let title = "This is home";   	
         return (         
